b50a4d69860ce2ae2e9bbea0a66be0f5295683e3
amedtech-creator <amedtech@gmail.com>
2026-04-20 09:23:11 +0900

    Add files via upload

--- sw-KY.js
+++ sw-KY.js
@@ -1,5 +1,5 @@
-// KY 주문 시스템 Service Worker v20260419-v14
-const CACHE = 'ky-order-v20260419-v14';
+// KY 주문 시스템 Service Worker v20260419-v15
+const CACHE = 'ky-order-v20260419-v15';
 const APP_ROOT = '/KY';
 const ASSETS = [
   APP_ROOT + '/',
